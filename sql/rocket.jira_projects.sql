94cb9cff36066b4850016fdad11003112140a302
berni-rocket <bernardo.colorado@therocketcode.com>
2025-01-27 17:14:12 -0600

    revisando nuevo layout

--- sql/rocket.jira_projects.sql
+++ sql/rocket.jira_projects.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS rocket.jira_projects;
 CREATE TABLE rocket.jira_projects (
-    project_id TEXT PRIMARY KEY,
-    project_key TEXT NOT NULL,
-    project_name TEXT NOT NULL,
-    project_type_key TEXT,
-    project_category_id TEXT,
-    project_category_name TEXT,
-    lead_account_id TEXT,
-    lead_display_name TEXT,
-    is_private BOOLEAN,
-    simplified BOOLEAN
+    project_id TEXT PRIMARY KEY, 
+    project_key TEXT NOT NULL, 
+    project_name TEXT NOT NULL, 
+    project_type_key TEXT, 
+    project_category_id TEXT, 
+    project_category_name TEXT, 
+    lead_account_id TEXT, 
+    lead_display_name TEXT, 
+    is_private BOOLEAN, 
+    simplified BOOLEAN 
 );
