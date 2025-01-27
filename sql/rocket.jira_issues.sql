69208c19522a9cefc5436a2f96e9956b74e646f6
berni-rocket <bernardo.colorado@therocketcode.com>
2025-01-27 15:48:50 -0600

    agregando nuevos campos a entidad issue

--- sql/rocket.jira_issues.sql
+++ sql/rocket.jira_issues.sql
@@ -1,18 +1,40 @@
 DROP TABLE IF EXISTS rocket.jira_issues;
-CREATE TABLE jira_issues ( 
-    issue_id TEXT PRIMARY KEY, 
+CREATE TABLE rocket.jira_issues ( 
+    --tarea
+    issue_id INT PRIMARY KEY, 
     issue_key TEXT NOT NULL, 
+    -- proyecto
+    project_id INT NOT NULL,
     project_key TEXT NOT NULL, 
-    created_date TIMESTAMP WITH TIME ZONE, 
-    updated_date TIMESTAMP WITH TIME ZONE, 
-    summary TEXT, 
-    description TEXT, 
+    
+    --usuario que crea la tarea
+    creator_account_id TEXT, 
+    creator_display_name TEXT,
+
+    -- usuario asignado
     assignee_account_id TEXT, 
     assignee_display_name TEXT, 
-    status_name TEXT, 
-    priority_name TEXT, 
+
+    -- usuario que reporta
     reporter_account_id TEXT, 
     reporter_display_name TEXT, 
-    creator_account_id TEXT, 
-    creator_display_name TEXT 
+
+    -- textos
+    summary TEXT, 
+    description TEXT, 
+    -- prioridad
+    priority_id INT, 
+    priority_name TEXT,
+
+    --tiempos
+    time_estimate INT,
+    due_date TIMESTAMP WITH TIME ZONE,
+    resolution_date TIMESTAMP WITH TIME ZONE,
+
+    -- estado
+    status_id INT,
+    status_name TEXT, 
+    -- marcas de tiempo
+    created_date TIMESTAMP WITH TIME ZONE, 
+    updated_date TIMESTAMP WITH TIME ZONE 
 );
--- src/lib/parsers/issues.py
+++ src/lib/parsers/issues.py
@@ -7,20 +7,52 @@ def issue_parser(data:dict)->dict:
   if 'priority' not in data['_airbyte_data']['fields'].keys():
     data['_airbyte_data']['fields']['priority'] = {}
   
-  return {
+  dict_data = {
+    
+      #tarea
       'issue_id': data['_airbyte_data']['id'],
       'issue_key': data['_airbyte_data']['key'],
+      
+      #proyecto
+      'project_id': data['_airbyte_data']['fields']['project']['id'],
       'project_key': data['_airbyte_data']['fields']['project']['key'],
-      'created_date': data['_airbyte_data']['fields']['created'],
-      'updated_date': data['_airbyte_data']['fields']['updated'],
-      'summary': data['_airbyte_data']['fields']['summary'],
-      'description': json.dumps(data['_airbyte_data']['fields'].get('description', {})),  # Manejar descripciones vacías
-      'assignee_account_id': data['_airbyte_data']['fields']['assignee'].get('accountId', None),  # Manejar asignados nulos
-      'assignee_display_name': data['_airbyte_data']['fields']['assignee'].get('displayName', None),
-      'status_name': data['_airbyte_data']['fields']['status']['name'],
-      'priority_name': data['_airbyte_data']['fields']['priority'].get('name',None),
-      'reporter_account_id': data['_airbyte_data']['fields']['reporter']['accountId'],
-      'reporter_display_name': data['_airbyte_data']['fields']['reporter']['displayName'],
+      
+      #usuario que crea la tarea
       'creator_account_id': data['_airbyte_data']['fields']['creator']['accountId'],
       'creator_display_name': data['_airbyte_data']['fields']['creator']['displayName'],
+      
+      #usuario asignado a la tarea
+      'assignee_account_id': data['_airbyte_data']['fields']['assignee'].get('accountId', None),  
+      'assignee_display_name': data['_airbyte_data']['fields']['assignee'].get('displayName', None),
+      
+      #usuario que reporta la tarea
+      'reporter_account_id': data['_airbyte_data']['fields']['reporter']['accountId'],
+      'reporter_display_name': data['_airbyte_data']['fields']['reporter']['displayName'],
+
+      #textos
+      'summary': data['_airbyte_data']['fields']['summary'],
+      'description': json.dumps(data['_airbyte_data']['fields'].get('description', {})), 
+      
+      #tiempos
+      'time_estimate': data['_airbyte_data']['fields'].get('timeestimate',0),
+      'duedate' : data['_airbyte_data']['fields'].get('duedate', None),
+      'resolution_date': data['_airbyte_data']['fields'].get('resolutiondate', None),
+
+      #estados
+      'status_id': data['_airbyte_data']['fields']['status']['id'],
+      'status_name': data['_airbyte_data']['fields']['status']['name'],
+      
+      #prioridades
+      'priority_id': data['_airbyte_data']['fields']['priority'].get('id',None),
+      'priority_name': data['_airbyte_data']['fields']['priority'].get('name',None),
+
+      #fechas de creacion y actualizacion
+      'created_date': data['_airbyte_data']['fields']['created'],
+      'updated_date': data['_airbyte_data']['fields']['updated'],
   }
+
+  #formateamos fecha de vencimiento
+  if dict_data['duedate'] is not None:
+    dict_data['duedate']=dict_data['duedate']+'T18:00:00.000-0600'
+  
+  return dict_data
